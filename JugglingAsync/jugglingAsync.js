48f0759061a983b299f46af7cc0d62dfe2614431
OscarSierra24 <oscar_sierra_02@hotmail.com>
2018-07-14 13:44:57 -0400

    better jugglingAsync

--- JugglingAsync/jugglingAsync.js
+++ JugglingAsync/jugglingAsync.js
@@ -3,36 +3,21 @@ var URLs = [process.argv[2], process.argv[3], process.argv[4]]
 var messages = new Array(3).fill("")
 var finished = new Array(3).fill(false)
 
-http.get(URLs[0],(res) =>{
-    res.on("data", (data)=>{
-        messages[0] += data 
-    })
-    res.on("end", ()=>{
-        finished[0] = true
-        messagesCompleted(finished) ? printMessages(messages) : null
-    })
-})
+for(let i = 0; i < 3; i++){
+    httpGet(i)
+}
 
-http.get(URLs[1],(res) =>{
-    res.on("data", (data)=>{
-        messages[1] += data 
+function httpGet(i){
+    http.get(URLs[i], (res) =>{
+        res.on("data", (data)=>{
+            messages[i] += data
+        })
+        res.on("end", ()=>{
+            finished[i] = true
+            messagesCompleted(finished) ? printMessages(messages) : null
+        })
     })
-    res.on("end", ()=>{
-        finished[1] = true
-        messagesCompleted(finished) ? printMessages(messages) : null
-    })
-})
-
-http.get(URLs[2],(res) =>{
-    res.on("data", (data)=>{
-        messages[2] += data 
-    })
-    res.on("end", ()=>{
-        finished[2] = true
-        messagesCompleted(finished) ? printMessages(messages) : null
-    })
-})
-
+}
 
 function messagesCompleted(finished){
     for(let i=0; i<finished.length; i++){
