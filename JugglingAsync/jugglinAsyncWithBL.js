b8bacfd31cb91ad27aced7f24a5215f6964fff39
OscarSierra24 <oscar_sierra_02@hotmail.com>
2018-07-14 13:39:42 -0400

    2

---
+++ JugglingAsync/jugglinAsyncWithBL.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+for(var i = 0; i<3; i++){
+    httpGet(i)
+}
+
+function httpGet(index){
+    http.get(process.argv[2 + index], (res)=>{
+        res.pipe(bl((err, data)=>{
+            if(err){
+                return console.error(err)
+            }
+
+            results[index] = data.toString()
+            count++
+
+            if(count === 3){
+                printResults()
+            }
+        }))   
+    })
+}
+
+function printResults(){
+    results.forEach((result)=>{
+        console.log(result)
+    })
+}
